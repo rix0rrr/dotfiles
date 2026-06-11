b831f5a59c44f450dcc8d0dc9e854e71a3704a02
Rico Huijbers <rix0rrr@gmail.com>
2026-06-11 09:14:15 +0200

    Add comment

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -25,6 +25,7 @@ vim.keymap.set({ "n" }, "<D-}>", ":bn<CR>")
 -- <D-p> opens (LazyVim) file finder
 vim.keymap.set({ "n" }, "<D-p>", ",ff", { remap = true })
 
+-- <C-s> saves both in UI as well as (Kitty) terminal
 vim.keymap.set({ "n", "i", "v" }, "<D-s>", function()
   vim.cmd.write()
 end, { desc = "Save" })
